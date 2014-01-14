e32b5d0a1471062cf662dad45be3b5e5d40e4ec1
maerdogan <maerdo@gmail.com>
2014-01-14 01:31:57 +0100

    project files are created
    basic things like directories (inc/, css/ and inc/) and files (index,
    header, footer) are created.

---
+++ inc/header.php
@@ -0,0 +1,10 @@
+<<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>project-id-005</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Nunito:400,300,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
